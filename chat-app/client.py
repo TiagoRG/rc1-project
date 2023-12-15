3354b2200abb79221424ac8df2b98efab3437e3a
TiagoRG <tiago.rgarcia@ua.pt>
2023-12-15 20:02:35 +0000

    Chat working, with fixed packets
    Signed-off-by: TiagoRG <tiago.rgarcia@ua.pt>

--- chat-app/client.py
+++ chat-app/client.py
@@ -9,27 +9,28 @@ import readline
 
 
 def message_send_handler():
+    global sock
     global username
-    order = 0
     print("\n----------------------------------------")
     while True:
         try:
             message = input()
             print("\033[A                                                 \033[A")
             if message == '':
-                print("\033[A                                                 \033[A")
+                print(
+                    "\033[A                                                 \033[A")
                 print("----------------------------------------")
                 continue
-            message = f"{username}: {message}".encode()
-            version = 1
-            order += 1
-            size = len(message)
+            message = message.encode()
+            usr_size = len(username)
+            msg_size = len(message)
             try:
                 now = int(datetime.datetime.now().timestamp())
             except ValueError:
                 now = 0
-            pkt = struct.pack('!BLLL{}s'.format(
-                size), version, size, order, now, message)
+            pkt = struct.pack('!LLL{}s{}s'.format(usr_size, msg_size),
+                              usr_size, msg_size, now,
+                              username, message)
             sock.send(pkt)
         except (socket.timeout, socket.error):
             print('Server error. Done!')
@@ -38,7 +39,8 @@ def message_send_handler():
 
 
 def message_receive_handler():
-    headerformat = '!BLLL'
+    global sock
+    headerformat = '!LLL'
     while True:
         try:
             request = sock.recv(struct.calcsize(headerformat))
@@ -48,49 +50,70 @@ def message_receive_handler():
                 print('Server error. Done!')
                 os.kill(os.getpid(), signal.SIGTERM)
                 sys.exit(0)
-            # version, size, order, now = pktheader
-            size, now = pktheader[1], pktheader[3]
-            request = sock.recv(size)
-            pktdata = struct.unpack('{}s'.format(size), request)
+            usr_size, msg_size, now = pktheader
+            request = sock.recv(usr_size + msg_size)
+            pktdata = struct.unpack('{}s{}s'.format(
+                usr_size, msg_size), request)
             print()
             print("\033[A                                                 \033[A")
             print("\033[A                                                 \033[A")
             print("\033[A                                                 \033[A")
-            print("[{}] {}\n"
+            print("[{}] {}: {}\n"
                   .format(
                       datetime.datetime.fromtimestamp(
                           now).strftime('%Y-%m-%d %H:%M:%S'),
-                      pktdata[0].decode()))
+                      pktdata[0].decode(),
+                      pktdata[1].decode()))
             print("----------------------------------------")
             sys.stdout.write(readline.get_line_buffer())
             sys.stdout.flush()
-            # print("\rReceived ver: {}, size: {}, order: {}, date: {} -> {}\n\nMessage > "
-            #       .format(version, size, order, now, pktdata[0].decode()), end='')
         except (socket.timeout, socket.error):
             print('Server error. Done!')
             os.kill(os.getpid(), signal.SIGTERM)
 
 
-send_handler = threading.Thread(target=message_send_handler)
-receive_handler = threading.Thread(target=message_receive_handler)
-
-
 def signal_handler(sig, frame):
+    global sock
     print('\nDone!')
+    sock.close()
     os.kill(os.getpid(), signal.SIGTERM)
 
 
-signal.signal(signal.SIGINT, signal_handler)
-username = input('Enter your username: ')
-print('Logged in as {}.'.format(username))
-print('Press Ctrl+C to exit...\n\n')
+def main():
+    global sock
+    global username
 
-ip_addr = "127.0.0.1"
-tcp_port = 5005
+    signal.signal(signal.SIGINT, signal_handler)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip_addr = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
+    tcp_port = int(sys.argv[2]) if len(sys.argv) > 2 else 5005
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.connect((ip_addr, tcp_port))
+    try:
+        sock.connect((ip_addr, tcp_port))
+    except socket.error:
+        print('Cannot connect to server. Done!')
+        os.kill(os.getpid(), signal.SIGTERM)
+        sys.exit(0)
 
-send_handler.start()
-receive_handler.start()
+    username = input('Enter your username: ').encode()
+    headerformat = '!L'
+
+    # Send list of connected clients
+    sock.send(struct.pack('!L{}s'.format(len(username)),
+                          len(username), username))
+
+    request = sock.recv(struct.calcsize(headerformat))
+    list_size = struct.unpack(headerformat, request)[0]
+    list_of_clients = sock.recv(list_size).decode()
+
+    print('Connected Clients: {}'.format(list_of_clients))
+    print('Logged in as {}.'.format(username.decode()))
+    print('Press Ctrl+C to exit...\n\n')
+
+    threading.Thread(target=message_send_handler).start()
+    threading.Thread(target=message_receive_handler).start()
+
+
+if __name__ == '__main__':
+    main()
--- chat-app/server.py
+++ chat-app/server.py
@@ -7,25 +7,10 @@ import struct
 import datetime
 
 
-SOCKS_LIST = []
-
-
-def signal_handler(sig, frame):
-    print('\nDone!')
-    server.close()
-    os.kill(os.getpid(), signal.SIGTERM)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-print('Press Ctrl+C to exit...')
-
-##
-
-
 def handle_client_connection(client_socket, address):
-    global SOCKS_LIST
+    global CLIENTS
     print('Accepted connection from {}:{}'.format(address[0], address[1]))
-    headerformat = '!BLLL'
+    headerformat = '!LLL'
     try:
         while True:
             request = client_socket.recv(struct.calcsize(headerformat))
@@ -34,48 +19,120 @@ def handle_client_connection(client_socket, address):
             except struct.error:
                 print('Client {} error. Done!'.format(address))
                 break
-            version, size, order, now = pktheader
-            request = client_socket.recv(size)
-            pktdata = struct.unpack('{}s'.format(size), request)
-            # data = f"{address[0]}:{address[1]}: {pktdata[0].decode()}"
-            data = pktdata[0].decode()
-            size = len(data)
-            print('Received ver: {}, size: {}, order: {}, date: {} -> {}'
-                  .format(version, size, order,
-                          datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S'),
-                          data))
+            username_size, message_size, now = pktheader
+            CLIENTS[client_socket]["order"] += 1
+            request = client_socket.recv(username_size + message_size)
+            pktdata = struct.unpack('{}s{}s'.format(
+                username_size, message_size), request)
+            username = pktdata[0].decode()
+            message = pktdata[1].decode()
+            print('Received: username_size: {}, message_size: {}, order: {}, date: {} -> {}: {}'
+                  .format(username_size, message_size,
+                          CLIENTS[client_socket]["order"],
+                          datetime.datetime.fromtimestamp(
+                              now).strftime('%Y-%m-%d %H:%M:%S'),
+                          username, message))
             try:
-                pkt = struct.pack('!BLLL{}s'.format(size), version, size, order, now, data.encode())
+                pkt = struct.pack(
+                    '!LLL{}s{}s'.format(username_size, message_size),
+                    username_size, message_size, now,
+                    username.encode(), message.encode())
             except struct.error:
                 print('Client {} error. Done!'.format(address))
                 break
-            # client_socket.send(pkt)
-            for sock in SOCKS_LIST:
+            for sock in CLIENTS.keys():
                 sock.send(pkt)
     except (socket.timeout, socket.error):
         print('Client {} error. Done!'.format(address))
     finally:
         client_socket.close()
-        SOCKS_LIST.remove(client_socket)
-        print(SOCKS_LIST)
+        CLIENTS.pop(client_socket)
+        print(CLIENTS)
         print('Closed connection from {}:{}'.format(address[0], address[1]))
 
 
-ip_addr = "0.0.0.0"
-tcp_port = 5005
+def handle_connections():
+    global server
+    global CLIENTS
+
+    while True:
+        client_sock, address = server.accept()
+
+        # Get username
+        headerformat = '!L'
+        request = client_sock.recv(struct.calcsize(headerformat))
+        username_size = struct.unpack(headerformat, request)[0]
+        username = client_sock.recv(username_size).decode()
+        print('Received username: {}'.format(username))
+        CLIENTS[client_sock] = {"username": username, "order": 0}
+
+        # Send list of connected clients
+        list_of_clients = ', '.join([user["username"]
+                                     for user in CLIENTS.values()]).encode()
+        client_sock.send(struct.pack('!L{}s'.format(
+            len(list_of_clients)), len(list_of_clients), list_of_clients))
+
+        print(CLIENTS)
+        client_handler = threading.Thread(
+            target=handle_client_connection, args=(client_sock, address),
+            daemon=True)
+        client_handler.start()
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((ip_addr, tcp_port))
-server.listen(5)  # max backlog of connections
+def handle_commands():
+    global server
+    global CLIENTS
+    while True:
+        command = input()
+        if command == 'exit':
+            server.close()
+            os.kill(os.getpid(), signal.SIGTERM)
+        elif command == 'list':
+            print('Connected clients:\n')
+            for sock in CLIENTS.keys():
+                print('Client username: {}\n    {}\n    {}\n'.format(
+                    CLIENTS[sock]["username"],
+                    sock.getpeername(),
+                    CLIENTS[sock]["order"]))
+        else:
+            print('Unknown command.')
 
-print('Listening on {}:{}'.format(ip_addr, tcp_port))
 
-while True:
-    client_sock, address = server.accept()
-    SOCKS_LIST.append(client_sock)
-    print(SOCKS_LIST)
-    client_handler = threading.Thread(
-        target=handle_client_connection, args=(client_sock, address),
-        daemon=True)
-    client_handler.start()
+def signal_handler(sig, frame):
+    global server
+    print('\nDone!')
+    server.close()
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+def main():
+    global server
+    global CLIENTS
+    CLIENTS = {}
+
+    ip_addr = "0.0.0.0"
+    try:
+        tcp_port = int(sys.argv[1]) if len(sys.argv) > 1 else 5005
+    except ValueError:
+        print('Invalid port number.')
+        sys.exit(0)
+
+    print('Starting server on {}:{}'.format(ip_addr, tcp_port))
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ip_addr, tcp_port))
+        server.listen(5)  # max backlog of connections
+    except socket.error as e:
+        print('Error initializing server: {}'.format(e))
+        sys.exit(0)
+
+    print('Listening on {}:{}'.format(ip_addr, tcp_port))
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C to exit...')
+
+    threading.Thread(target=handle_connections).start()
+    threading.Thread(target=handle_commands).start()
+
+
+if __name__ == '__main__':
+    main()
