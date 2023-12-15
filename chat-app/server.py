60bf107985a037f993775c7ecde91dd64a3851e4
TiagoRG <tiago.rgarcia@ua.pt>
2023-12-15 21:31:50 +0000

    Add colors and formatting to server output
    Signed-off-by: TiagoRG <tiago.rgarcia@ua.pt>

--- chat-app/server.py
+++ chat-app/server.py
@@ -1,10 +1,28 @@
 import os
+import re
+import sys
 import socket
 import threading
 import signal
-import sys
 import struct
 import datetime
+
+
+class Color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = "\033[93m"
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
+class LogMessage:
+    INFO = "[INFO]   "
+    ERROR = "[ERROR]  "
+    WARNING = "[WARNING]"
+    CONNECTION = "[CLIENT] "
+    MESSAGE = "[MESSAGE]"
 
 
 def handle_client_connection(client, address):
@@ -21,7 +39,8 @@ def handle_client_connection(client, address):
     list_of_clients = ', '.join([user["username"] for user in CLIENTS.values()]).encode()
     client.send(struct.pack('!L{}s'.format(len(list_of_clients)), len(list_of_clients), list_of_clients))
 
-    print('Accepted connection from {} ({}:{})'.format(username, address[0], address[1]))
+    print('{}[{}] {}{} Accepted connection from {} ({}:{})'.format(Color.OKGREEN, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                                   LogMessage.CONNECTION, Color.ENDC, username, address[0], address[1]))
     headerformat = '!LL'
     try:
         while True:
@@ -29,28 +48,30 @@ def handle_client_connection(client, address):
             try:
                 pktheader = struct.unpack(headerformat, request)
             except struct.error:
-                print('Client {} error. Done!'.format(address))
+                # print('{}[{}] {}{} Client {} error.'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC, address))
                 break
             message_size, now = pktheader
             CLIENTS[client]["order"] += 1
             request = client.recv(message_size)
             pktdata = struct.unpack('{}s'.format(message_size), request)
             message = pktdata[0].decode()
-            print('Received: order: {}, message_size: {}, date: {} -> {}: {}'
-                  .format(CLIENTS[client]["order"], message_size, datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S'), username, message))
+            print('{}[{}] {}{} order: {}, message_size: {}, date: {} -> {}: {}'
+                  .format(Color.HEADER, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.MESSAGE, Color.ENDC,
+                      CLIENTS[client]["order"], message_size, datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S'), username, message))
             try:
                 pkt = struct.pack('!LLL{}s{}s'.format(username_size, message_size), username_size, message_size, now, username.encode(), message.encode())
             except struct.error:
-                print('Client {} error. Done!'.format(address))
+                # print('{}[{}] {}{} Client {} error.'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC, address))
                 break
             for sock in CLIENTS.keys():
                 sock.send(pkt)
     except (socket.timeout, socket.error):
-        print('Client {} error. Done!'.format(address))
+        print('{}[{}] {}{} Client {} error.'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC, address))
     finally:
         client.close()
         CLIENTS.pop(client)
-        print('Closed connection from {} ({}:{})'.format(username, address[0], address[1]))
+        print('{}[{}] {}{} Closed connection from {} ({}:{})'
+              .format(Color.WARNING, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.CONNECTION, Color.ENDC, username, address[0], address[1]))
 
 
 def handle_connections():
@@ -68,15 +89,16 @@ def handle_commands():
     global CLIENTS
     while True:
         command = input()
-        if command == 'exit':
+        if re.match(r'^[eq]$', command):
+            print('{}[{}] {}{} Shutting down server...'.format(Color.WARNING, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.INFO, Color.ENDC))
             server.close()
             os.kill(os.getpid(), signal.SIGTERM)
-        elif command == 'list':
+        elif re.match(r'^[lc]$', command):
             print('Connected clients:\n')
             for sock in CLIENTS.keys():
-                print('Client username: {}\n    {}\n    {}\n'.format(CLIENTS[sock]["username"], sock.getpeername(), CLIENTS[sock]["order"]))
+                print('Username: {}\nAddress: {}:{}\nOrders: {}\n'.format(CLIENTS[sock]["username"], sock.getpeername()[0], sock.getpeername()[1], CLIENTS[sock]["order"]))
         else:
-            print('Unknown command.')
+            print('{}[{}] {}{} Unknown command.'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC))
 
 
 def signal_handler(sig, frame):
@@ -95,21 +117,21 @@ def main():
     try:
         tcp_port = int(sys.argv[1]) if len(sys.argv) > 1 else 5005
     except ValueError:
-        print('Invalid port number.')
+        print('{}[{}] {}{} Invalid port number.'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC))
         sys.exit(0)
 
-    print('Starting server on {}:{}'.format(ip_addr, tcp_port))
+    print('{}[{}] {}{} Starting server on {}:{}'.format(Color.OKBLUE, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.INFO, Color.ENDC, ip_addr, tcp_port))
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((ip_addr, tcp_port))
         server.listen(5)  # max backlog of connections
     except socket.error as e:
-        print('Error initializing server: {}'.format(e))
+        print('{}[{}] {}{} Error initializing server: {}'.format(Color.FAIL, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.ERROR, Color.ENDC, e))
         sys.exit(0)
 
-    print('Listening on {}:{}'.format(ip_addr, tcp_port))
+    print('{}[{}] {}{} Listening on {}:{}'.format(Color.OKBLUE, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.INFO, Color.ENDC, ip_addr, tcp_port))
     signal.signal(signal.SIGINT, signal_handler)
-    print('Press Ctrl+C to exit...')
+    print('{}[{}] {}{} Press Ctrl+C to exit...'.format(Color.OKBLUE, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), LogMessage.INFO, Color.ENDC))
 
     threading.Thread(target=handle_connections).start()
     threading.Thread(target=handle_commands).start()
